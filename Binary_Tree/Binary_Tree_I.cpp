000fc0f8ed2923d66bbfbae2cfc7ed945b983e0c
Sameerchakare56 <sameerchakravedi75@gmail.com>
2026-05-06 21:27:12 +0530

    c

--- Binary_Tree/Binary_Tree_I.cpp
+++ Binary_Tree/Binary_Tree_I.cpp
@@ -30,8 +30,24 @@ Node* buildTree(vector<int> preorder){
 
     return root;
 }
+
+void preOrder(Node* root){
+    if(root == NULL){
+        return ;
+    }
+    cout << root -> data << endl;
+    preOrder(root-> left);
+    preOrder(root -> right);
+}
+
 int main(){
     vector<int> preorder = { 1 ,2,-1 , -1, 3 ,4,-1,-1,5,-1,-1};
-   
+    Node* root = buildTree(preorder);
+    preOrder(root);
+    cout << endl;
+    cout << root -> data << endl;
+    cout << root -> left -> data << endl;
+    cout << root -> right  -> data << endl;
+
     return 0;
 }
